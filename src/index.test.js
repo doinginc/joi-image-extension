c4e59220d17a09d97ac1d16ad410dfe46ad41a31
chrisforrette <chris@chrisforrette.com>
2018-01-03 17:29:49 -0800

    Adding tests and ironing out test scripts

--- src/index.test.js
+++ src/index.test.js
@@ -19,26 +19,289 @@ describe('Joi Image Extension', function () {
     return Promise.all(images.map(image => readFileAsync(path.join(__dirname, image))))
   }
 
-  it('should fail for an image under width minimum', async function () {
-    const images = await getTestImages()
+  it('should fail for images below width minimum', async function () {
     const schema = Joi
       .image()
       .minWidth(150)
 
+    const images = await getTestImages()
+
     images.forEach(image => {
       const result = schema.validate(image)
       assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.minWidth')
     })
-    console.log('images', images)
   })
 
-  it('should pass for an image equal to width minimum')
+  it('should pass for images equal to width minimum', async function () {
+    const schema = Joi
+      .image()
+      .minWidth(100)
 
-  it('should pass for an image above width minimum')
+    const images = await getTestImages()
 
-  it('should fail for an image under width maximum')
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
 
-  it('should pass for an image equal to width maximum')
+  it('should pass for images above width minimum', async function () {
+    const schema = Joi
+      .image()
+      .minWidth(99)
 
-  it('should pass for an image above width maximum')
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images below width maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxWidth(150)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images equal to width maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxWidth(100)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should fail for images above width maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxWidth(99)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.maxWidth')
+    })
+  })
+
+  it('should fail for images below height minimum', async function () {
+    const schema = Joi
+      .image()
+      .minHeight(150)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.minHeight')
+    })
+  })
+
+  it('should pass for images equal to height minimum', async function () {
+    const schema = Joi
+      .image()
+      .minHeight(100)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images above height minimum', async function () {
+    const schema = Joi
+      .image()
+      .minHeight(99)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images below height maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxHeight(150)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images equal to height maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxHeight(100)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should fail for images above height maximum', async function () {
+    const schema = Joi
+      .image()
+      .maxHeight(99)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.maxHeight')
+    })
+  })
+
+  it('should fail for images below minimum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .minDimensions(150, 150)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.minDimensions')
+    })
+  })
+
+  it('should pass for images equal to minimum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .minDimensions(100, 100)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images above minimum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .minDimensions(99, 99)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images below maximum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .maxDimensions(150, 150)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should pass for images equal to maximum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .maxDimensions(100, 100)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNull(result.error)
+    })
+  })
+
+  it('should fail for images above maximum dimensions', async function () {
+    const schema = Joi
+      .image()
+      .maxDimensions(99, 99)
+
+    const images = await getTestImages()
+
+    images.forEach(image => {
+      const result = schema.validate(image)
+      assert.isNotNull(result.error)
+      assert.equal(result.error.details[0].type, 'image.maxDimensions')
+    })
+  })
+
+  it('should pass for images in the specified allowed types', async function () {
+    const schema = Joi
+      .image()
+      .allowTypes('jpg')
+
+    const image = await readFileAsync(path.join(__dirname, '../test/100x100.jpg'))
+
+    const result = schema.validate(image)
+    assert.isNull(result.error)
+  })
+
+  it('should fail for images not in the specified allowed types', async function () {
+    const schema = Joi
+      .image()
+      .allowTypes(['jpg', 'png'])
+
+    const image = await readFileAsync(path.join(__dirname, '../test/100x100.gif'))
+
+    const result = schema.validate(image)
+    assert.isNotNull(result.error)
+    assert.equal(result.error.details[0].type, 'image.allowTypes')
+  })
+
+  it('should fail for images in the specified disallowed types', async function () {
+    const schema = Joi
+      .image()
+      .disallowTypes('jpg')
+
+    const image = await readFileAsync(path.join(__dirname, '../test/100x100.jpg'))
+
+    const result = schema.validate(image)
+    assert.isNotNull(result.error)
+    assert.equal(result.error.details[0].type, 'image.disallowTypes')
+  })
+
+  it('should pass for images not in the specified disallowed types', async function () {
+    const schema = Joi
+      .image()
+      .disallowTypes(['png'])
+
+    const image = await readFileAsync(path.join(__dirname, '../test/100x100.jpg'))
+
+    const result = schema.validate(image)
+    assert.isNull(result.error)
+  })
 })
